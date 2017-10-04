abc102a6fb84cdea5e9b21bdb3fafd38cb74af32
wandyersilva <wandyer.s@gmail.com>
2017-10-03 22:54:46 -0300

    - Correções da animação
    - Criação de perfis do projeto para facilitar desenvolvimento e deploy em produção

--- src/main/java/io/lunes/temp/TempApplication.java
+++ src/main/java/io/lunes/temp/TempApplication.java
@@ -5,30 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
-/*
-	Production
- */
-//@SpringBootApplication
-//public class TempApplication extends SpringBootServletInitializer {
-//
-//	@Override
-//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-//		return application.sources(TempApplication.class);
-//	}
-//
-//	public static void main(String[] args) throws Exception {
-//		SpringApplication.run(TempApplication.class, args);
-//	}
-//
-//}
-
-/*
-	Development
- */
 @SpringBootApplication
-public class TempApplication {
+public class TempApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TempApplication.class);
+	}
+
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(TempApplication.class, args);
 	}
+
 }
